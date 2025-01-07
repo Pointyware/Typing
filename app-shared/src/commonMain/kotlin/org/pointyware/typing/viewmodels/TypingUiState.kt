f1e4ce5a70bb46e43ea711a18a5f65d0ce75bbab
Taush Sampley <taushsampley@gmail.com>
2025-01-07 11:36:53 -0600

    Add progress, wpm, and timerState to Typing Ui State

--- app-shared/src/commonMain/kotlin/org/pointyware/typing/viewmodels/TypingUiState.kt
+++ app-shared/src/commonMain/kotlin/org/pointyware/typing/viewmodels/TypingUiState.kt
@@ -7,9 +7,18 @@ import org.pointyware.typing.ui.LoadingState
  */
 data class TypingUiState(
     val subject: String = "",
+    val progress: String = "",
+    val wpm: Float = 0.0f,
+    val timerState: TimerUiState,
     val loadingState: LoadingState = LoadingState.Idle,
 ) {
     companion object {
-        val Empty = TypingUiState()
+        val Empty = TypingUiState(
+            subject = "",
+            progress = "",
+            wpm = 0.0f,
+            timerState = TimerUiState.Stopped,
+            loadingState = LoadingState.Idle,
+        )
     }
 }
