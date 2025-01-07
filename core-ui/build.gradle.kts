089dcd589456979534eecebd83c7f8ce5f14a3cd
Taush Sampley <taushsampley@gmail.com>
2025-01-06 19:42:13 -0600

    Fix core-ui module using hyphen for ios framework

--- core-ui/build.gradle.kts
+++ core-ui/build.gradle.kts
@@ -25,7 +25,7 @@ kotlin {
         iosSimulatorArm64()
     ).forEach {
         it.binaries.framework {
-            baseName = "core-ui"
+            baseName = "core_ui"
             isStatic = true
         }
     }
