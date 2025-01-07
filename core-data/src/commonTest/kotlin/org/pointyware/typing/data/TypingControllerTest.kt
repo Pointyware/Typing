7152740609b4cdec1dcedb15c76391c54a86a4f6
Taush Sampley <taushsampley@gmail.com>
2025-01-07 13:16:31 -0600

    Create typing controller test

---
+++ core-data/src/commonTest/kotlin/org/pointyware/typing/data/TypingControllerTest.kt
@@ -0,0 +1,51 @@
+package org.pointyware.typing.data
+
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+/**
+ *
+ */
+class TypingControllerTest {
+
+    private lateinit var controller: TypingController
+
+    @BeforeTest
+    fun setUp() {
+        controller = TypingControllerImpl()
+    }
+
+    @AfterTest
+    fun tearDown() {
+
+    }
+
+    @Test
+    fun incorrect_input_should_generate_incorrect_ranges() {
+        /*
+        Given: the typing controller has a subject
+        And: the progress state is observed
+         */
+        val subject = "the quick brown fox jumped over the lazy dog"
+        val progress = controller.progress
+
+        /*
+        When: the user types an incorrect character
+         */
+        "the quick borwn fox jumped over the laztdog".forEach {
+            controller.consume(it)
+        }
+
+        /*
+        Then: the progress should contain the incorrect range
+         */
+        val result = progress.value
+        val expected = TypingProgress(
+            "the quick borwn fox jumped over the laztdog",
+            listOf(11..12, 39..42)
+        )
+        assertEquals(expected, result)
+    }
+}
