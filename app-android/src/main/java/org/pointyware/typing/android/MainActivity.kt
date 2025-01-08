52910c66cc0fe3b1245f5521cc951c21af4f030c
Taush Sampley <taushsampley@gmail.com>
2025-01-07 18:28:47 -0600

    provide nav controller in android app

--- app-android/src/main/java/org/pointyware/typing/android/MainActivity.kt
+++ app-android/src/main/java/org/pointyware/typing/android/MainActivity.kt
@@ -3,23 +3,16 @@ package org.pointyware.typing.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
 import org.pointyware.typing.ui.TypingApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TypingApp()
-        }
-    }
-}
 
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        TypingApp()
+        val navController = NavHostController(this)
+        setContent {
+            TypingApp(navController)
+        }
     }
 }
