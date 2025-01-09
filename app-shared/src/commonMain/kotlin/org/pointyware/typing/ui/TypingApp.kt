0db76767a30df0994c68d17d349517930e46067e
Taush Sampley <taushsampley@gmail.com>
2025-01-09 16:56:13 -0600

    remove unused import

--- app-shared/src/commonMain/kotlin/org/pointyware/typing/ui/TypingApp.kt
+++ app-shared/src/commonMain/kotlin/org/pointyware/typing/ui/TypingApp.kt
@@ -7,7 +7,6 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.toRoute
 import org.pointyware.typing.data.TestSubjectProvider
 import org.pointyware.typing.data.TypingControllerImpl
 import org.pointyware.typing.navigation.Screen
