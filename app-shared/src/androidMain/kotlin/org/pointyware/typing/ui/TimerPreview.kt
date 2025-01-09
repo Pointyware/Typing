4c7ea4a4d38932970ff83cddf7311bfbf9cf5019
Taush Sampley <taushsampley@gmail.com>
2025-01-09 17:14:28 -0600

    Use preview parameter provider to simplify previes

--- app-shared/src/androidMain/kotlin/org/pointyware/typing/ui/TimerPreview.kt
+++ app-shared/src/androidMain/kotlin/org/pointyware/typing/ui/TimerPreview.kt
@@ -2,54 +2,32 @@ package org.pointyware.typing.ui
 
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import org.pointyware.typing.ui.theme.TypingTheme
 import org.pointyware.typing.viewmodels.TimerUiState
+
+class TimerStateProvider: PreviewParameterProvider<TimerUiState> {
+    override val values = sequenceOf(
+        TimerUiState.Hidden,
+        TimerUiState.Stopped,
+        TimerUiState.Running(
+            timeRemaining = 1000
+        )
+    )
+}
 
 /**
  */
 @LightDarkModePreview
 @Composable
 fun StoppedTimerPreview(
-    modifier: Modifier = Modifier,
+    @PreviewParameter(TimerStateProvider::class) state: TimerUiState
 ) {
     Surface {
         TypingTheme {
             Timer(
-                state = TimerUiState.Stopped
-            )
-        }
-    }
-}
-/**
- */
-@LightDarkModePreview
-@Composable
-fun HiddenTimerPreview(
-    modifier: Modifier = Modifier,
-) {
-    Surface {
-        TypingTheme {
-            Timer(
-                state = TimerUiState.Hidden
-            )
-        }
-    }
-}
-
-/**
- */
-@LightDarkModePreview
-@Composable
-fun RunningTimerPreview(
-    modifier: Modifier = Modifier,
-) {
-    Surface {
-        TypingTheme {
-            Timer(
-                state = TimerUiState.Running(
-                    timeRemaining = 1000
-                )
+                state = state
             )
         }
     }
