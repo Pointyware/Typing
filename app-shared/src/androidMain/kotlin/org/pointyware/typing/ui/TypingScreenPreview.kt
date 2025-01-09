b1416bace2b70c12f74cf112d22d2dfc0a88d662
Taush Sampley <taushsampley@gmail.com>
2025-01-09 17:32:14 -0600

    preview typing view across light/dark and devices

---
+++ app-shared/src/androidMain/kotlin/org/pointyware/typing/ui/TypingScreenPreview.kt
@@ -0,0 +1,36 @@
+package org.pointyware.typing.ui
+
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import org.pointyware.typing.data.TypingProgress
+import org.pointyware.typing.ui.theme.TypingTheme
+import org.pointyware.typing.viewmodels.TimerUiState
+import org.pointyware.typing.viewmodels.TypingUiState
+
+/**
+ *
+ */
+@LightDarkModePreview
+@TestDevicePreviews
+@Composable
+fun TypingScreenPreview() {
+    TypingTheme {
+        Surface {
+            TypingView(
+                state = TypingUiState(
+                    subject = "Some subject",
+                    progress = TypingProgress(
+                        string = "some subject",
+                        incorrect = listOf(0..0),
+                        wpm = 80.0f,
+                        accuracy = 0.9f
+                    ),
+                    loadingState = LoadingState.Idle,
+                    timerState = TimerUiState.Running(timeRemaining = 15)
+                ),
+                onInputChange = {},
+                onReset = {}
+            )
+        }
+    }
+}
