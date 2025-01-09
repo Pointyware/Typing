b235fc9ec5c5b3fa2ae42894c618b0fd4e5e0f17
Taush Sampley <taushsampley@gmail.com>
2025-01-09 15:41:15 -0600

    Document typing progress members

--- core-data/src/commonMain/kotlin/org/pointyware/typing/data/TypingProgress.kt
+++ core-data/src/commonMain/kotlin/org/pointyware/typing/data/TypingProgress.kt
@@ -1,6 +1,21 @@
 package org.pointyware.typing.data
 
 data class TypingProgress(
+    /**
+     * The current input string.
+     */
     val string: String,
-    val incorrect: List<IntRange>
+    /**
+     * The range of incorrect characters in the input when compared to the current typing subject.
+     */
+    val incorrect: List<IntRange>,
+    /**
+     * The average words per minute calculated using an average word length of 5 characters.
+     */
+    val wpm: Float,
+    /**
+     * The fraction of correct characters in the input when compared to the current typing subject.
+     * In the range [0, 1].
+     */
+    val accuracy: Float,
 )
