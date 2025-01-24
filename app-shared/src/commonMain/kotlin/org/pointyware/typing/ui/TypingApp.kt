7b8fe6c19f26e63d4e43d7f0f1e5612631f7fd05
Taush Sampley <taushsampley@gmail.com>
2025-01-24 11:21:22 -0600

    Fix OCR typo

--- app-shared/src/commonMain/kotlin/org/pointyware/typing/ui/TypingApp.kt
+++ app-shared/src/commonMain/kotlin/org/pointyware/typing/ui/TypingApp.kt
@@ -36,7 +36,7 @@ fun TypingApp(
         composable<Screen.Typing> {
             val viewModel = remember {
                 val subjectProvider = TestSubjectProvider(
-                    "There lived in a certain village a poor old woman who had collected a mess of beans, and was going to cook them. So she made a fire on her hearth, and, in order to make it bmm better, she put in a handful of straw. When the beans began to bubble in the pot, one of them fell out and lay, never noticed, near a straw which was already there; soon a red-hot coal jumped out of the fire and joined the pair."
+                    "There lived in a certain village a poor old woman who had collected a mess of beans, and was going to cook them. So she made a fire on her hearth, and, in order to make it burn better, she put in a handful of straw. When the beans began to bubble in the pot, one of them fell out and lay, never noticed, near a straw which was already there; soon a red-hot coal jumped out of the fire and joined the pair."
                 ) // GrimmSubjectProvider()
                 val controller = TypingControllerImpl(subjectProvider)
                 controller.reset()
