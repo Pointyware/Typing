e4349e1d1e50d21491ec75329dc2903a23cfe0a7
Taush Sampley <taushsampley@gmail.com>
2025-01-23 22:35:57 -0600

    Use test subject provider to just get the daily challenge out of the way augh

--- app-shared/src/commonMain/kotlin/org/pointyware/typing/ui/TypingApp.kt
+++ app-shared/src/commonMain/kotlin/org/pointyware/typing/ui/TypingApp.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import org.pointyware.typing.data.TestSubjectProvider
 import org.pointyware.typing.data.TypingControllerImpl
 import org.pointyware.typing.navigation.Screen
 import org.pointyware.typing.typing.GrimmSubjectProvider
@@ -34,7 +35,9 @@ fun TypingApp(
         }
         composable<Screen.Typing> {
             val viewModel = remember {
-                val subjectProvider = GrimmSubjectProvider()
+                val subjectProvider = TestSubjectProvider(
+                    "There lived in a certain village a poor old woman who had collected a mess of beans, and was going to cook them. So she made a fire on her hearth, and, in order to make it bmm better, she put in a handful of straw. When the beans began to bubble in the pot, one of them fell out and lay, never noticed, near a straw which was already there; soon a red-hot coal jumped out of the fire and joined the pair."
+                ) // GrimmSubjectProvider()
                 val controller = TypingControllerImpl(subjectProvider)
                 controller.reset()
                 TypingViewModelImpl(controller)
