4713ff912594bff5e012ef8915efcfd8ad26cc06
Taush Sampley <taushsampley@gmail.com>
2025-01-25 21:05:52 -0600

    Move android src files to androidMain

--- app-android/build.gradle.kts
+++ app-android/build.gradle.kts
@@ -1,8 +1,28 @@
 plugins {
     alias(libs.plugins.androidApplication)
-    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.compose.compiler)
+}
+
+kotlin {
+    androidTarget() {
+
+    }
+
+    sourceSets {
+        val androidMain by getting {
+            dependencies {
+                implementation(projects.appShared)
+                implementation(libs.androidx.composeUi)
+                implementation(libs.androidx.composePreview)
+                implementation(libs.androidx.composeMaterial3)
+                implementation(libs.androidx.activityCompose)
+
+                implementation(compose.components.resources)
+            }
+        }
+    }
 }
 
 android {
@@ -47,20 +67,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
-    implementation(projects.appShared)
-    implementation(libs.androidx.composeUi)
-    implementation(libs.androidx.composePreview)
-    implementation(libs.androidx.composeMaterial3)
-    implementation(libs.androidx.activityCompose)
     debugImplementation(libs.androidx.composeTooling)
-
-    implementation(compose.components.resources)
 }
 
 compose.resources {
