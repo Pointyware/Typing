382a392eb96e638f92f1c289b8fa56d9be3492c1
Taush Sampley <taushsampley@gmail.com>
2025-01-29 22:07:41 -0600

    Provide typing controller binding in core-data module

--- core-data/src/commonMain/kotlin/org/pointyware/typing/data/di/DataModule.kt
+++ core-data/src/commonMain/kotlin/org/pointyware/typing/data/di/DataModule.kt
@@ -1,10 +1,16 @@
 package org.pointyware.typing.data.di
 
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
+import org.pointyware.typing.data.TypingController
+import org.pointyware.typing.data.TypingControllerImpl
 
 /**
  *
  */
 fun dataModule() = module {
-
+    singleOf(::TypingControllerImpl) {
+        bind<TypingController>()
+    }
 }
