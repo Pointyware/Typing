a3a448ef745432f81debf7a3cfa5ff75e8706a83
Taush Sampley <taushsampley@gmail.com>
2025-01-29 22:07:58 -0600

    provide typing view model binding in app-shared module

--- app-shared/src/commonMain/kotlin/org/pointyware/typing/di/SharedModule.kt
+++ app-shared/src/commonMain/kotlin/org/pointyware/typing/di/SharedModule.kt
@@ -1,10 +1,24 @@
 package org.pointyware.typing.di
 
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
+import org.pointyware.typing.viewmodels.TypingViewModel
+import org.pointyware.typing.viewmodels.TypingViewModelImpl
 
 /**
  *
  */
 fun sharedAppModule() = module {
+
+    includes(
+        sharedViewModelModule()
+    )
+}
+
+fun sharedViewModelModule() = module {
+    singleOf(::TypingViewModelImpl) {
+        bind<TypingViewModel>()
+    }
 
 }
