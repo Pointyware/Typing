617732ae4d4868e36a77d4a06fe4a75f0df6ae12
Taush Sampley <taushsampley@gmail.com>
2025-02-01 21:25:38 -0600

    Add typing/data modules to koin module hierarchy

--- app-shared/src/commonMain/kotlin/org/pointyware/typing/di/SharedModule.kt
+++ app-shared/src/commonMain/kotlin/org/pointyware/typing/di/SharedModule.kt
@@ -3,6 +3,8 @@ package org.pointyware.typing.di
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
+import org.pointyware.typing.data.di.dataModule
+import org.pointyware.typing.typing.di.typingModule
 import org.pointyware.typing.viewmodels.TypingViewModel
 import org.pointyware.typing.viewmodels.TypingViewModelImpl
 
@@ -12,7 +14,11 @@ import org.pointyware.typing.viewmodels.TypingViewModelImpl
 fun sharedAppModule() = module {
 
     includes(
-        sharedViewModelModule()
+        sharedViewModelModule(),
+
+        typingModule(),
+
+        dataModule()
     )
 }
 
