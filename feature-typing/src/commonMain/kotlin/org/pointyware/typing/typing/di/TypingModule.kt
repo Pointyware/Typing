52eedd03f52136b29370f2c3c1112a348eff6b22
Taush Sampley <taushsampley@gmail.com>
2025-02-02 22:17:31 -0600

    provide qualified uri string to grimm subject provider

--- feature-typing/src/commonMain/kotlin/org/pointyware/typing/typing/di/TypingModule.kt
+++ feature-typing/src/commonMain/kotlin/org/pointyware/typing/typing/di/TypingModule.kt
@@ -1,16 +1,29 @@
 package org.pointyware.typing.typing.di
 
-import org.koin.core.module.dsl.bind
-import org.koin.core.module.dsl.singleOf
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import org.pointyware.typing.data.SubjectProvider
 import org.pointyware.typing.typing.GrimmSubjectProvider
+import org.pointyware.typing.typing.Res
+
+
+internal val stories_uri = named("stories-uri")
 
 /**
  *
  */
+@OptIn(ExperimentalResourceApi::class)
 fun typingModule() = module {
-    singleOf(::GrimmSubjectProvider) {
-        bind<SubjectProvider>()
+    single<SubjectProvider> {
+        GrimmSubjectProvider(
+            get<String>(qualifier = stories_uri)
+        )
+    }
+
+    factory<String>(qualifier = stories_uri) {
+        // Res.getUri("files/android-story.json")
+        // Res.getUri("files/desktop-stories.json")
+        Res.getUri("files/grimm-stories.json")
     }
 }
