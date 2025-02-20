f741134b80fb5afa817f65721ed9ef82fcafdfcc
Taush Sampley <taushsampley@gmail.com>
2025-02-20 17:38:48 -0600

    replace buttons with item selectors

--- app-shared/src/commonMain/kotlin/org/pointyware/typing/ui/MainMenuScreen.kt
+++ app-shared/src/commonMain/kotlin/org/pointyware/typing/ui/MainMenuScreen.kt
@@ -1,12 +1,13 @@
 package org.pointyware.typing.ui
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import org.pointyware.typing.viewmodels.MainMenuUiState
 import org.pointyware.typing.viewmodels.MainMenuViewModel
@@ -42,17 +43,29 @@ fun MainMenuView(
     Column(
         modifier = modifier
     ) {
-        Button(
-            onClick = { onStartTyping(state.selectedVocab?.id ?: 0) },
-            enabled = state.selectedVocab != null,
-        ) {
-            Text("Words")
-        }
-        Button(
-            onClick = { onStartTyping(state.selectedParagraphs?.id ?: 0) },
-            enabled = state.selectedParagraphs != null,
-        ) {
-            Text("Paragraphs")
-        }
+        var selectedWordIndex: Int? by remember { mutableStateOf(null) }
+        ItemSelector(
+            prompt = "Words",
+            items = state.vocabList.map { it.fileUriString },
+            onItemSelected = { selectedWordIndex = it; onSelectWords(it) },
+            selectedItem = selectedWordIndex,
+            onAction = action@ {
+                val index = state.selectedVocab ?: return@action
+                val id = state.vocabList.getOrNull(index)?.id ?: return@action
+                onStartTyping(id)
+            }
+        )
+        var selectedParagraphIndex: Int? by remember { mutableStateOf(null) }
+        ItemSelector(
+            prompt = "Paragraphs",
+            items = state.storiesList.map { it.fileUriString },
+            onItemSelected = { selectedParagraphIndex = it; onSelectParagraphs(it) },
+            selectedItem = state.selectedVocab,
+            onAction = action@ {
+                val index = state.selectedParagraphs ?: return@action
+                val id = state.storiesList.getOrNull(index)?.id ?: return@action
+                onStartTyping(id)
+            }
+        )
     }
 }
