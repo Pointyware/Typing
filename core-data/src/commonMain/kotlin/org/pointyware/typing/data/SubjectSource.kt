0fed51f8cea66c7d391a1eec70c81bde5f4ed954
Taush Sampley <taushsampley@gmail.com>
2025-02-20 14:12:50 -0600

    Add int id to subject sources

--- core-data/src/commonMain/kotlin/org/pointyware/typing/data/SubjectSource.kt
+++ core-data/src/commonMain/kotlin/org/pointyware/typing/data/SubjectSource.kt
@@ -7,10 +7,30 @@ import kotlinx.serialization.Serializable
  */
 @Serializable
 sealed interface SubjectSource {
-
+    val id: Int
 }
 
 @Serializable
 data class FileUri(
+    override val id: Int,
     val fileUriString: String
 ): SubjectSource
+
+val vocabUri = FileUri(0, "files/vocab.json")
+val storiesUri = FileUri(1, "files/stories.json")
+object SubjectSourceRegistry {
+
+    private val map: MutableMap<Int, SubjectSource> = mutableMapOf()
+    init {
+        put(vocabUri)
+        put(storiesUri)
+    }
+
+    fun put(subjectSource: SubjectSource) {
+        map[subjectSource.id] = subjectSource
+    }
+
+    fun get(id: Int): SubjectSource? {
+        return map[id]
+    }
+}
