bb8f54596e12c296b6c9f09ff0d90c72e0394f9f
Taush Sampley <taushsampley@gmail.com>
2025-02-20 16:55:49 -0600

    Add load() function to main menu view model

--- app-shared/src/commonMain/kotlin/org/pointyware/typing/viewmodels/MainMenuViewModel.kt
+++ app-shared/src/commonMain/kotlin/org/pointyware/typing/viewmodels/MainMenuViewModel.kt
@@ -1,10 +1,16 @@
 package org.pointyware.typing.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import org.pointyware.typing.interactors.LoadSubjectsUseCase
+import org.pointyware.typing.ui.LoadingState
 
 /**
  * Represents a UI state for the main menu screen.
@@ -12,6 +18,7 @@ import kotlinx.coroutines.flow.update
 interface MainMenuViewModel {
     val state: StateFlow<MainMenuUiState>
 
+    fun load()
     fun onSelectWords(index: Int)
     fun onSelectParagraphs(index: Int)
 }
@@ -20,12 +27,36 @@ interface MainMenuViewModel {
  *
  */
 class MainMenuViewModelImpl(
-
+    private val loadSubjectsUseCase: LoadSubjectsUseCase
 ): ViewModel(), MainMenuViewModel {
 
     private val _state = MutableStateFlow(MainMenuUiState.Empty)
     override val state: StateFlow<MainMenuUiState>
         get() = _state.asStateFlow()
+
+    private var loadingJob: Job? = null
+    override fun load() {
+        loadingJob?.cancel("Loading job restarted")
+        loadingJob = viewModelScope.launch {
+            loadSubjectsUseCase()
+                .onSuccess { loadedSubjects ->
+                    _state.update {
+                        it.copy(
+                            vocabList = loadedSubjects.vocabList,
+                            storiesList = loadedSubjects.storiesList
+                        )
+                    }
+                }
+                .onFailure {  error ->
+                    _state.update {
+                        it.copy(
+                            loadingState = LoadingState.Error(error.message ?: "Unknown error")
+                        )
+                    }
+                }
+            loadingJob = null
+        }
+    }
 
     override fun onSelectWords(index: Int) {
         _state.update {
