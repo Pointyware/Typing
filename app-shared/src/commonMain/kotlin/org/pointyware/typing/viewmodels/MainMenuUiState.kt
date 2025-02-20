b06ded5994b55976809c597fbd07da4ed272e454
Taush Sampley <taushsampley@gmail.com>
2025-02-20 16:54:09 -0600

    Add main menu loading state

--- app-shared/src/commonMain/kotlin/org/pointyware/typing/viewmodels/MainMenuUiState.kt
+++ app-shared/src/commonMain/kotlin/org/pointyware/typing/viewmodels/MainMenuUiState.kt
@@ -1,12 +1,14 @@
 package org.pointyware.typing.viewmodels
 
 import org.pointyware.typing.data.FileUri
+import org.pointyware.typing.ui.LoadingState
 
 data class MainMenuUiState(
     val vocabList: List<FileUri>,
     val selectedVocab: FileUri?,
     val storiesList: List<FileUri>,
     val selectedParagraphs: FileUri?,
+    val loadingState: LoadingState
 ) {
     companion object {
         val Empty = MainMenuUiState(
@@ -14,6 +16,7 @@ data class MainMenuUiState(
             selectedVocab = null,
             storiesList = emptyList(),
             selectedParagraphs = null,
+            loadingState = LoadingState.Idle
         )
     }
 }
