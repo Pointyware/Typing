8466d8e213073e8be3b061c1a46ae557786b7003
Taush Sampley <taushsampley@gmail.com>
2025-02-20 12:35:18 -0600

    make subject source serializable

--- core-data/src/commonMain/kotlin/org/pointyware/typing/data/SubjectSource.kt
+++ core-data/src/commonMain/kotlin/org/pointyware/typing/data/SubjectSource.kt
@@ -1,12 +1,16 @@
 package org.pointyware.typing.data
 
+import kotlinx.serialization.Serializable
+
 /**
  *
  */
+@Serializable
 sealed interface SubjectSource {
 
 }
 
+@Serializable
 data class FileUri(
     val fileUriString: String
 ): SubjectSource
