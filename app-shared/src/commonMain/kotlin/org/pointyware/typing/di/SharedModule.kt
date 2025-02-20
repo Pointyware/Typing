2d310849edbd71bc1116b1f1d86ba4f648a2e80a
Taush Sampley <taushsampley@gmail.com>
2025-02-20 17:44:31 -0600

    Fix missing interactor in koin moduels

--- app-shared/src/commonMain/kotlin/org/pointyware/typing/di/SharedModule.kt
+++ app-shared/src/commonMain/kotlin/org/pointyware/typing/di/SharedModule.kt
@@ -2,9 +2,11 @@ package org.pointyware.typing.di
 
 import kotlinx.coroutines.MainScope
 import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 import org.pointyware.typing.data.di.dataModule
+import org.pointyware.typing.interactors.LoadSubjectsUseCase
 import org.pointyware.typing.typing.di.typingModule
 import org.pointyware.typing.viewmodels.MainMenuViewModel
 import org.pointyware.typing.viewmodels.MainMenuViewModelImpl
@@ -37,4 +39,6 @@ fun sharedViewModelModule() = module {
     singleOf(::MainMenuViewModelImpl) {
         bind<MainMenuViewModel>()
     }
+
+    factoryOf(::LoadSubjectsUseCase)
 }
