fedeb8ff38ea667d90127c11b5f8deff30b843d8
Taush Sampley <taushsampley@gmail.com>
2025-02-20 12:39:03 -0600

    provide main menu view model

--- app-shared/src/commonMain/kotlin/org/pointyware/typing/di/SharedModule.kt
+++ app-shared/src/commonMain/kotlin/org/pointyware/typing/di/SharedModule.kt
@@ -1,10 +1,13 @@
 package org.pointyware.typing.di
 
+import kotlinx.coroutines.MainScope
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 import org.pointyware.typing.data.di.dataModule
 import org.pointyware.typing.typing.di.typingModule
+import org.pointyware.typing.viewmodels.MainMenuViewModel
+import org.pointyware.typing.viewmodels.MainMenuViewModelImpl
 import org.pointyware.typing.viewmodels.TypingViewModel
 import org.pointyware.typing.viewmodels.TypingViewModelImpl
 
@@ -29,5 +32,9 @@ fun sharedViewModelModule() = module {
 
     single<TypingViewModel> {
         get<TypingViewModelImpl>()
+    }
+
+    singleOf(::MainMenuViewModelImpl) {
+        bind<MainMenuViewModel>()
     }
 }
