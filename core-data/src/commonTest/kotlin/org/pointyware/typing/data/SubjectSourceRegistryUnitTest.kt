b12663ee733aca2d82469566df2ca244b0ed07f4
Taush Sampley <taushsampley@gmail.com>
2025-02-21 13:30:36 -0600

    start and stop koin

--- core-data/src/commonTest/kotlin/org/pointyware/typing/data/SubjectSourceRegistryUnitTest.kt
+++ core-data/src/commonTest/kotlin/org/pointyware/typing/data/SubjectSourceRegistryUnitTest.kt
@@ -2,6 +2,11 @@ package org.pointyware.typing.data
 
 import kotlinx.coroutines.test.runTest
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.pointyware.typing.data.di.dataModule
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -9,11 +14,25 @@ import kotlin.test.assertEquals
  */
 class SubjectSourceRegistryUnitTest {
 
+    @BeforeTest
+    fun setUp() {
+        startKoin {
+            modules(
+                dataModule(),
+            )
+        }
+    }
+
+    @AfterTest
+    fun tearDown() {
+        stopKoin()
+    }
+
     @OptIn(ExperimentalResourceApi::class)
     @Test
     fun registryLoadsFileContents() = runTest {
         SubjectSourceRegistry.loadFrom(
-            Res.readBytes(""),
+            Res.readBytes("files/directory.json"),
             { Res.getUri("files/words/$it.json") },
             { Res.getUri("files/paragraphs/$it.json") }
         )
