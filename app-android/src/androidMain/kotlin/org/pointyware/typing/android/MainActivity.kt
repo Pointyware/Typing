b17eb705df4dfa815d02b7f81927fe593f66b7b7
Taush Sampley <taushsampley@gmail.com>
2025-02-21 09:27:51 -0600

    Test opening resources from Android

--- app-android/src/androidMain/kotlin/org/pointyware/typing/android/MainActivity.kt
+++ app-android/src/androidMain/kotlin/org/pointyware/typing/android/MainActivity.kt
@@ -1,16 +1,19 @@
 package org.pointyware.typing.android
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.NavHostController
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.pointyware.typing.typing.GrimmSubjectProvider
 import org.pointyware.typing.ui.TypingApp
 import org.pointyware.typing.ui.theme.TypingTheme
+import java.io.File
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalResourceApi::class)
+
+    private val logTag = this::class.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,6 +26,20 @@ class MainActivity : ComponentActivity() {
                     navController
                 )
             }
+        }
+    }
+
+    @OptIn(ExperimentalResourceApi::class)
+    override fun onResume() {
+        super.onResume()
+
+        val uri = Res.getUri("files/android-story.json")
+        Log.i(logTag, "uri: $uri")
+
+        val assetPath = uri.substringAfter("file:///android_asset/")
+        assets.open(assetPath).use {
+            val storyString = it.reader().readText()
+            Log.i(logTag, "storyString: $storyString")
         }
     }
 }
