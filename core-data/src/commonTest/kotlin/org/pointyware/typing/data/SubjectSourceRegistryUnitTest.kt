d2192adb79b309d0e140452b0a57ee23e24147ad
Taush Sampley <taushsampley@gmail.com>
2025-02-21 12:43:28 -0600

    add unit test expectations

--- core-data/src/commonTest/kotlin/org/pointyware/typing/data/SubjectSourceRegistryUnitTest.kt
+++ core-data/src/commonTest/kotlin/org/pointyware/typing/data/SubjectSourceRegistryUnitTest.kt
@@ -1,12 +1,15 @@
 package org.pointyware.typing.data
 
 import kotlinx.coroutines.test.runTest
+import org.jetbrains.compose.resources.ExperimentalResourceApi
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 /**
  */
 class SubjectSourceRegistryUnitTest {
 
+    @OptIn(ExperimentalResourceApi::class)
     @Test
     fun registryLoadsFileContents() = runTest {
         SubjectSourceRegistry.loadFrom(
@@ -14,5 +17,12 @@ class SubjectSourceRegistryUnitTest {
             { Res.getUri("files/words/$it.json") },
             { Res.getUri("files/paragraphs/$it.json") }
         )
+
+        assertEquals(FileUri(0, "files/words/alpha.json"), SubjectSourceRegistry.get(0))
+        assertEquals(FileUri(1, "files/words/beta.json"), SubjectSourceRegistry.get(1))
+        assertEquals(FileUri(2, "files/words/delta.json"), SubjectSourceRegistry.get(2))
+        assertEquals(FileUri(3, "files/paragraphs/gamma.json"), SubjectSourceRegistry.get(3))
+        assertEquals(FileUri(4, "files/paragraphs/phi.json"), SubjectSourceRegistry.get(4))
+        assertEquals(FileUri(5, "files/paragraphs/theta.json"), SubjectSourceRegistry.get(5))
     }
 }
