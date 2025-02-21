609a4e74c502964555fa00e1eb6ae73bad1a692f
Taush Sampley <taushsampley@gmail.com>
2025-02-21 14:53:01 -0600

    create utility koin getter for data coroutine scope

--- core-data/src/commonMain/kotlin/org/pointyware/typing/data/di/DataModule.kt
+++ core-data/src/commonMain/kotlin/org/pointyware/typing/data/di/DataModule.kt
@@ -5,8 +5,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.serialization.json.Json
-import org.koin.core.module.dsl.singleOf
 import org.koin.core.qualifier.named
+import org.koin.core.scope.Scope
 import org.koin.dsl.module
 import org.pointyware.typing.data.SubjectProviderFactory
 import org.pointyware.typing.data.SubjectProviderFactoryImpl
@@ -15,13 +15,14 @@ import org.pointyware.typing.data.TypingController
 import org.pointyware.typing.data.TypingControllerImpl
 
 val dataScope = named("data-scope")
+val Scope.dataCoroutineScope get() = get<CoroutineScope>(qualifier = dataScope)
 
 /**
  *
  */
 fun dataModule() = module {
     single<TypingController> {
-        TypingControllerImpl(get(), get(qualifier = dataScope))
+        TypingControllerImpl(get(), dataCoroutineScope)
     }
 
     singleOf(::SubjectSourceRegistry)
